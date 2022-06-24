92da49bbdaacf5b8da75005a04dfd6a1f9b3b8d5
Caleb Garratt <caleb.garratt@roamdigital.com>
2022-06-25 11:09:03 +1200

    Commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <WorldMap/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import WorldMap from './components/WorldMap.vue';
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.column {
+  display: block;
+  height: 188px;
+  width: 100%;
+  /* align-items: center; */
+}
+
+.columnContainer {
+  display: flex;
+  justify-content: center;
+}
+
+.node {
+  width: 140px;
+  height: 140px;
+  display: inline-block;
+  /* background-color: #d5d57b; */
+  border-radius: 50px;
+  border: 4px solid #0000;
+  margin: 20px;
+}
+
+.node.hoverable:hover {
+  border-color: white;
+  cursor: pointer;
+}
+
+.node.mountain {
+  background-color: #d5d57b;
+  background-image: url('./assets/rock.jpg');
+}
+
+.node.forest {
+  background-color: green;
+  background-image: url('./assets/forest.jpg');
+}
+
+.node.sea {
+  background-color: blue;
+  background-image: url('./assets/sea.jpg');
+}
+
+.node.village {
+  background-color: grey;
+  background-image: url('./assets/hay.jpg');
+}
+
+.view {
+  /* background: rgb(69,54,33);
+  background: linear-gradient(4deg, rgba(69,54,33,1) 0%, rgba(181,181,160,1) 65%, rgba(247,248,215,1) 91%, rgba(252,255,211,1) 100%);  */
+  background-image: url('./assets/map-texture.jpg');
+  /* background-color: beige; */
+  width: 600px;
+  height: 600px;
+  border-radius: 50px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center; 
 }
 </style>
---
+++ src/components/WorldMap.vue
@@ -0,0 +1,50 @@
+<template>
+    <h1>Yet another Xianxia</h1>
+    <div class="view">
+        <span class="column" v-for="(row, rowIter) in map" :key="rowIter">
+            <span class="columnContainer">
+                <span
+                    :class="[
+                        'node',
+                        node.class ? node.class + ' hoverable' : '',
+                        selected[0] === rowIter && selected[1] === nodeIter ? 'selected' : ''
+                    ]"
+                    v-for="(node, nodeIter) in row"
+                    :key="nodeIter"
+                    @click="clickNode"
+                ></span>
+            </span>
+        </span>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const E = {};
+const M = {
+    class: 'mountain'
+};
+const F = {
+    class: 'forest'
+};
+const S = {
+    class: 'sea'
+};
+const V = {
+    class: 'village'
+}
+const map = [
+    [E, E, F],
+    [M, M, S],
+    [E, V, E]
+];
+
+const selected = ref([-1,-1]);
+
+const clickNode = (rowIter, nodeIter) => {
+    console.log("clicked node");
+    selected.value = [rowIter, nodeIter];
+}
+
+</script>
